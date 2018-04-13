16d2f91aa146940cbbb40a0fa41f6cb41039eb3a
rabbbit <aaaasd_66589559@qq.com>
2018-04-13 14:44:40 +0800

    add base profile edtie

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Length
+
+class EditProfileForm(Form):
+    name = StringField('Real name', validators=[Length(0, 64)]) 
+    location = StringField('Location', validators=[Length(0, 64)]) 
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template, abort, render_template
+from flask import render_template, abort, render_template, flash, url_for, redirect
 from . import main
 from ..models import User
+from .forms import EditProfileForm
+from flask_login import current_user, login_required
+from .. import db
 
 @main.route('/')
 def index():
@@ -9,7 +12,24 @@ def index():
 
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first()
+    user = User.query.filter_by(name=username).first()
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        flash('updated ')
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        return redirect(url_for('.user', username=current_user.name))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
